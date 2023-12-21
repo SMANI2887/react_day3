2d1a3c7a6c140f68dba3773d3b4faea45aa5179c
MANIKANDAN S <smani2887@gmail.com>
2023-12-21 13:22:58 +0530

    ReactJS local Array passing

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+function App(props) {
+  //console.log(props)
+  console.log(props.notes);
+  const{notes} = props;
+
+  console.log(notes);
+  return (
+    <div>
+      <h1>Notes</h1>
+
+      <ul>
+        <li>{notes[0].content}</li>
+        <li>{notes[1].content}</li>
+        <li>{notes[2].content}</li>
+        <li>{notes[3].content}</li>
+        <li>{notes[4].content}</li>
+      </ul>
+
+    </div>
+  )
+}
+
+export default App
---
+++ src/main.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+
+const notes = [
+  {
+    id:1,
+    content:'working with Lists',
+    important:true
+  },
+  {
+    id:2,
+    content:'Usage of keys',
+    important:false
+  },
+  {
+    id:3,
+    content:'Conditional Rendering',
+    important:true
+  },
+  {
+    id:4,
+    content:'Handling Events',
+    important:true
+  },
+  {
+    id:5,
+    content:'Props Vs State',
+    important:true
+  }
+];
+ReactDOM.createRoot(document.getElementById('root')).render(<App notes={notes}/>)
